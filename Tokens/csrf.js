3b4e0645af777662bd566cb24934f75a95122516
RomanLandais <roman.landais@gmail.com>
2024-05-19 19:27:15 +0400

    save before changing token method

--- Tokens/csrf.js
+++ Tokens/csrf.js
@@ -9,6 +9,7 @@ function generateCSRFToken() {
 // Middleware pour générer et stocker le jeton CSRF dans la session utilisateur
 function generateCSRFTokenMiddleware(req, res, next) {
   req.session.csrfToken = generateCSRFToken();
+  console.log('CSRF token généré :', req.session.csrfToken);
   next();
 }
 
@@ -17,6 +18,8 @@ function csrfProtection(req, res, next) {
   const tokenFromRequest =
     req.body.csrfToken || req.query.csrfToken || req.headers['csrf-token'];
   const tokenFromSession = req.session.csrfToken;
+  console.log('Token CSRF de la requête :', tokenFromRequest);
+  console.log('Token CSRF de la session :', tokenFromSession);
 
   if (!tokenFromRequest || tokenFromRequest !== tokenFromSession) {
     return res.status(403).json({ error: 'Token CSRF invalide' });
